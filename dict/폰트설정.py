42c2ce8ed0e463c5149253129a91fef51b327d15
minanalysis <94737570+minanalysis@users.noreply.github.com>
2022-01-13 14:55:45 +0900

    Create 폰트설정.py

---
+++ dict/폰트설정.py
@@ -0,0 +1,33 @@
+# 윈도우 : "Malgun Gothic"
+# 맥 : "AppleGothic"
+def get_font_family():
+    """
+    시스템 환경에 따른 기본 폰트명을 반환하는 함수
+    """
+    import platform
+    system_name = platform.system()
+
+    if system_name == "Darwin" :
+        font_family = "AppleGothic"
+    elif system_name == "Windows":
+        font_family = "Malgun Gothic"
+    else:
+        # Linux(colab)
+        !apt-get install fonts-nanum -qq  > /dev/null
+        !fc-cache -fv
+
+        import matplotlib as mpl
+        mpl.font_manager._rebuild()
+        findfont = mpl.font_manager.fontManager.findfont
+        mpl.font_manager.findfont = findfont
+        mpl.backends.backend_agg.findfont = findfont
+        
+        font_family = "NanumBarunGothic"
+    return font_family
+
+# 폰트설정
+plt.rc("font", family=get_font_family())
+# 마이너스폰트 설정
+plt.rc("axes", unicode_minus=False)
+
+#출처: 오늘코드 박조은 강사님 
